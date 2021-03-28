2f3989cafccd7e6b9ef17d393527e76610008870
Taylor Shin <talezshin@gmail.com>
2021-03-28 15:36:04 -0300

    Removing unwanted stuffs.

--- docs/WinePak/WinePak_Install.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak remote-add --if-not-exists winepak https://dl.winepak.org/repo/winepak.flatpakrepo
-flatpak install winepak tld.domain.Application
