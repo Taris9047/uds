71582355303315ecf7a692e56dd9d79029798db9
Taylor Shin <talezshin@gmail.com>
2021-05-05 10:31:07 -0300

    Added Gcc-11 as gcc-latest
    Let's wait for other distributions to adopt gcc-11. If one of major ones
    adopt it on their LTS, we'll change the main gcc with 11.

---
+++ data/update_json.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+die() {
+	printf 'Error: %s\n' "$1"
+	exit 1
+}
+
+[ ! -x "$(command -v hjson)" ] && die "Oh crap! we need hjson command!"
+
+DIR="$(cd "$(dirname "$0")" && pwd)"
+
+hjson -j "$DIR/urls.hjson" >"$DIR/urls.json"
