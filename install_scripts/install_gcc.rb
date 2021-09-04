f139353edaec1b5488d5f007ac11169a12cd4648
Taylor Shin <talezshin@gmail.com>
2021-09-04 02:35:50 -0400

    "Added Gcc4.8.3 to work with Sentaurus"

--- install_scripts/install_gcc.rb
+++ install_scripts/install_gcc.rb
@@ -588,4 +588,147 @@ class InstGCC4 < InstGCC
 
     self.WriteInfo
   end
-end # class InstGCCOld
+end # class InstGCC4
+
+class InstGCC4Sen < InstGCC
+  def initialize(args)
+    args.each do |k, v|
+      instance_variable_set("@#{k}", v) unless v.nil?
+    end
+
+    super(args)
+
+    @pkgname = "gcc4-sentaurus"
+    @source_url = SRC_URL[@pkgname]
+    # Separating this gcc installation.
+    @prefix = File.join(@prefix, ".opt/#{@pkgname}")
+    @ver_source = SRC_VER[@pkgname]
+
+    @conf_options = [
+      "--enable-shared",
+      "--disable-boostrap",
+      "--enable-threads=posix",
+      "--disable-nls",
+      "--enable-default-pie",
+      "--disable-multilib",
+      "--enable-languages=c,c++",
+      "--libdir={prefix}/lib",
+      "--libexecdir={prefix}/lib",
+      "--build={target_arch}",
+      "--host={target_arch}",
+      "--target={target_arch}",
+    ]
+
+    @env = {
+      "CC" => "gcc",
+      "CXX" => "g++",
+      "CFLAGS" => "-w -O2 -std=gnu89 -fgnu89-inline -fomit-frame-pointer -pipe",
+      "CXXFLAGS" => "-w -O2 -std=gnu++11 -fomit-frame-pointer -pipe",
+    # "LDFLAGS" => "-Wl,-rpath={prefix}/lib -Wl,-rpath={prefix}/lib64",
+    }
+  end
+
+  def do_install
+    @pkginfo_file = File.join(@pkginfo_dir, @pkgname + ".info")
+
+    # Replace '{prefix}' on configure parameters.
+    @conf_options.each_with_index do |co, ind|
+      if co.include? "{prefix}"
+        @conf_options[ind] = co.gsub("{prefix}", @prefix)
+      end
+      if co.include? "{target_arch}"
+        @conf_options[ind] = co.gsub("{target_arch}", @os_type)
+      end
+    end
+    @env.each do |key, flag|
+      if flag.include? "{prefix}"
+        @env[key] = flag.gsub("{prefix}", @prefix)
+      end
+    end
+
+    if File.file?(@pkginfo_file)
+      puts "Oh, it seems gcc was already installed!! Skipping!!"
+      return 0
+    end
+
+    puts "Downloading src from #{@source_url}"
+    dl = Download.new(@source_url, @src_dir)
+    source_file = dl.GetPath()
+    fp = FNParser.new(source_file)
+    src_tarball_fname, src_tarball_bname = fp.name
+
+    extracted_src_dir = File.join(@build_dir, src_tarball_bname)
+    bld_dir = extracted_src_dir + "-build"
+    @src_build_dir = bld_dir
+
+    if Dir.exists?(extracted_src_dir)
+      puts "Extracted folder has been found. Using it!"
+    else
+      puts "Extracting..."
+      self.Run("tar xf " + source_file + " -C " + @build_dir)
+    end
+
+    unless Dir.exists?(@prefix)
+      FileUtils.mkdir_p(@prefix)
+    end
+
+    # Downloading prerequisites
+    puts extracted_src_dir
+    self.Run("cd " + File.realpath(extracted_src_dir) + " && " + "./contrib/download_prerequisites")
+
+    # Need to patch a few files.
+    puts ""
+    puts "Patching bugged files..."
+    patch_cmd = [
+      "sed -i -e 's/__attribute__/\\/\\/__attribute__/g' #{extracted_src_dir}/gcc/cp/cfns.h",
+      "sed -i 's/struct ucontext/ucontext_t/g' #{extracted_src_dir}/libgcc/config/i386/linux-unwind.h",
+      "sed -i '/#include <pthread.h>/a #include <signal.h>' #{extracted_src_dir}/libsanitizer/asan/asan_linux.cc",
+      "sed -i 's/__res_state \\*statp = (__res_state\\*)state\\;/struct __res_state \\*statp = (struct __res_state\\*)state\\;/g' #{extracted_src_dir}/libsanitizer/tsan/tsan_platform_linux.cc",
+    ]
+    # end
+    self.Run(patch_cmd.join(" && "))
+
+    # Let's build!!
+    unless Dir.exists?(bld_dir)
+      puts "Build dir missing... making one..."
+    else
+      puts "Build dir exists, cleaning up before work!!"
+      FileUtils.rm_rf(bld_dir)
+    end
+    FileUtils.mkdir_p(bld_dir)
+
+    if @need_sudo
+      inst_cmd = "sudo make install"
+    else
+      inst_cmd = "make install"
+    end
+
+    opts = ["--prefix=" + @prefix] + @conf_options
+    cmd = [
+      self.get_env_str,
+      "cd #{File.realpath(bld_dir)}",
+      "#{File.join(File.realpath(extracted_src_dir), "configure")} #{opts.join(" ")}",
+      "nice make -j #{@Processors.to_s}",
+      inst_cmd,
+    ]
+
+    brag_msg = %{
+*** This is totally deprecated software! ***
+*** If it breaks, it breaks... ***
+*** (This package was added solely due to Sentaurus 2015 Version) ***
+
+*** If it breaks, try installing ... ***
+ (Ubuntu) gcc-multilib libstdc++6:i386
+ (RHEL) libstdc++.i686
+*** But don't expect to be 100\% successful. ***
+}
+    puts brag_msg
+    sleep(2)
+
+    # Ok let's rock!
+    puts "Compiling (with #{@Processors} processors) and Installing ..."
+    self.RunInstall(env: @env, cmd: cmd.join(" && "))
+
+    self.WriteInfo
+  end
+end # class InstGCC4Sen
