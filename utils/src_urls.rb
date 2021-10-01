01db2f6e62defc3c373efc447703dc3709e426c3
Taylor Shin <talezshin@gmail.com>
2021-10-01 17:51:21 -0400

    Fixed stupid stuff happened on tcl.

--- utils/src_urls.rb
+++ utils/src_urls.rb
@@ -184,7 +184,7 @@ module SRC_VER
 
       if pkg_name == 'tcl'
         src_tarball_fname = SRC_URL['tcl'].split('/')[-1]
-        ver_str = src_tarball_fname.split('-')[0][2..-1]
+        ver_str = src_tarball_fname.split('-')[0][3..-1]
 
         return Version.new(ver_str.split('.'))
       end
