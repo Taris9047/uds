b1bbb8e869abc62df8d7d8fd7d635775ad1b63b1
Taylor Shin <talezshin@gmail.com>
2021-11-09 15:13:12 -0500

    "Added GCC4.2.0 and GCC3.3"

--- install_scripts/install_gcc.rb
+++ install_scripts/install_gcc.rb
@@ -444,6 +444,24 @@ class InstGCC9 < InstGCC
   end
 end # class InstGCC9
 
+
+#####################################################################
+#                                                                  #
+# Compatibility stuffs... for a bit old programs.                   #
+# -> In fact, replacing those program's libstdc++ with              #
+#    system libstdc++                                              #
+# works much better.                                               #
+#                                                                  #
+#####################################################################
+
+#####################################################################
+#                                                                  #
+# Old CUDA support                                                 #
+#                                                                  #
+# Old GCC compiler build scripts for Old CUDA...                    #
+#                                                                  #
+#####################################################################
+
 # Gcc4.8.5 --> matching version for cuda 6.5 (MBP 2008)
 class InstGCC4 < InstGCC
   def initialize(args)
@@ -588,6 +606,15 @@ class InstGCC4 < InstGCC
     self.WriteInfo
   end
 end # class InstGCC4
+
+
+#####################################################################
+#                                                                  #
+# Sentaurus 2015 support                                           #
+#                                                                  #
+# Old GCC compiler build scripts for Sentaurus 2015                 #
+#                                                                  #
+#####################################################################
 
 class InstGCC4Sen < InstGCC
   def initialize(args)
@@ -735,3 +762,328 @@ class InstGCC4Sen < InstGCC
     self.WriteInfo
   end
 end # class InstGCC4Sen
+
+
+#####################################################################
+#                                                                  #
+# Cadence support                                                  #
+#                                                                  #
+# Old GCC compiler build scripts for Cadence suite.                 #
+#                                                                  #
+#####################################################################
+
+# GCC 4.2.0 for Cadence
+class InstGCC4Cadence < InstGCC
+  def initialize(args)
+    args.each do |k, v|
+      instance_variable_set("@#{k}", v) unless v.nil?
+    end
+
+    super(args)
+
+    @pkgname = "gcc4-cadence"
+    @source_url = SRC_URL[@pkgname]
+    # Separating this gcc installation.
+    @prefix = File.join(@prefix, ".opt/#{@pkgname}")
+    @ver_source = SRC_VER[@pkgname]
+
+    @conf_options = [
+      #"--disable-bootstrap",
+      "--enable-threads=posix",
+      "--disable-checking",
+      "--with-system-zlib",
+      "--enable-__cxa_atexit",
+      "--disable-libunwind-exceptions",
+      "--without-gcj",
+      "--without-libjava",
+      "--disable-libgcj",
+      "--enable-languages=c,c++",
+      "--disable-multilib",
+      "--with-cpu=generic",
+      "--libdir={prefix}/lib",
+      "--libexecdir={prefix}/lib",
+      "--build={target_arch}",
+      "--host={target_arch}",
+      "--target={target_arch}",
+    ]
+
+    @env = {
+      "CC" => "gcc -std=gnu89",
+      "CXX" => "g++ -std=gnu++98",
+      "CFLAGS" => "-w -O2 -fomit-frame-pointer -pipe -fPIC",
+      "CXXFLAGS" => "-w -O2 -fomit-frame-pointer -pipe -fPIC",
+    }
+  end
+
+  def do_install
+    @pkginfo_file = File.join(@pkginfo_dir, @pkgname + ".info")
+
+    # Replace '{prefix}' on configure parameters.
+    @conf_options.each_with_index do |co, ind|
+      if co.include? "{prefix}"
+        @conf_options[ind] = co.gsub("{prefix}", @prefix)
+      end
+      if co.include? "{target_arch}"
+        @conf_options[ind] = co.gsub("{target_arch}", @os_type)
+      end
+    end
+    @env.each do |key, flag|
+      if flag.include? "{prefix}"
+        @env[key] = flag.gsub("{prefix}", @prefix)
+      end
+    end
+
+    if File.file?(@pkginfo_file)
+      puts "Oh, it seems gcc was already installed!! Skipping!!"
+      return 0
+    end
+
+    puts "Downloading src from #{@source_url}"
+    dl = Download.new(@source_url, @src_dir)
+    source_file = dl.GetPath()
+    fp = FNParser.new(source_file)
+    src_tarball_fname, src_tarball_bname = fp.name
+
+    extracted_src_dir = File.join(@build_dir, src_tarball_bname)
+    bld_dir = extracted_src_dir + "-build"
+    @src_build_dir = bld_dir
+
+    if Dir.exists?(extracted_src_dir)
+      puts "Extracted folder has been found. Using it!"
+    else
+      puts "Extracting..."
+      self.Run("tar xf " + source_file + " -C " + @build_dir)
+    end
+
+    unless Dir.exists?(@prefix)
+      FileUtils.mkdir_p(@prefix)
+    end
+
+    # Downloading prerequisites
+    puts extracted_src_dir
+    #self.Run("cd " + File.realpath(extracted_src_dir) + " && " + "./contrib/download_prerequisites")
+
+    # Need to patch a few files.
+    #puts ""
+    #puts "Patching bugged files..."
+    #patch_cmd = [
+    #  "sed -i -e 's/__attribute__/\\/\\/__attribute__/g' #{extracted_src_dir}/gcc/cp/cfns.h",
+    #  "sed -i 's/struct ucontext/ucontext_t/g' #{extracted_src_dir}/libgcc/config/i386/linux-unwind.h",
+    #  "sed -i '/#include <pthread.h>/a #include <signal.h>' #{extracted_src_dir}/libsanitizer/asan/asan_linux.cc",
+    #  "sed -i 's/__res_state \\*statp = (__res_state\\*)state\\;/struct __res_state \\*statp = (struct __res_state\\*)state\\;/g' #{extracted_src_dir}/libsanitizer/tsan/tsan_platform_linux.cc",
+    #]
+    # end
+    #self.Run(patch_cmd.join(" && "))
+
+    # Let's build!!
+    unless Dir.exists?(bld_dir)
+      puts "Build dir missing... making one..."
+    else
+      puts "Build dir exists, cleaning up before work!!"
+      FileUtils.rm_rf(bld_dir)
+    end
+    FileUtils.mkdir_p(bld_dir)
+
+    if @need_sudo
+      inst_cmd = "sudo make install"
+    else
+      inst_cmd = "make install"
+    end
+
+    opts = ["--prefix=" + @prefix] + @conf_options
+    cmd = [
+      self.get_env_str,
+      "cd #{File.realpath(bld_dir)}",
+      "#{File.join(File.realpath(extracted_src_dir), "configure")} #{opts.join(" ")}",
+      "nice make -j #{@Processors.to_s}",
+      inst_cmd,
+    ]
+
+    brag_msg = %{
+
+*** This is totally deprecated software! ***
+*** If it breaks, it breaks... ***
+*** (This package was added solely due to Cadence!!) ***
+
+}
+    puts brag_msg
+    sleep(2)
+
+    # Ok let's rock!
+    puts "Compiling (with #{@Processors} processors) and Installing ..."
+    self.RunInstall(env: @env, cmd: cmd.join(" && "))
+
+    self.WriteInfo
+  end
+end # class InstGCC4Cadence
+
+# GCC 3.3 for Cadence
+class InstGCC3Cadence < InstGCC
+  def initialize(args)
+    args.each do |k, v|
+      instance_variable_set("@#{k}", v) unless v.nil?
+    end
+
+    super(args)
+
+    @pkgname = "gcc3-cadence"
+    @source_url = SRC_URL[@pkgname]
+    # Separating this gcc installation.
+    @prefix = File.join(@prefix, ".opt/#{@pkgname}")
+    @ver_source = SRC_VER[@pkgname]
+
+    @conf_options = [
+      # "--disable-bootstrap",
+      "--enable-threads=posix",
+      "--disable-checking",
+      "--with-system-zlib",
+      "--enable-__cxa_atexit",
+      "--disable-libunwind-exceptions",
+      "--without-gcj",
+      "--without-libjava",
+      "--disable-libgcj",
+      "--enable-languages=c,c++",
+      "--disable-multilib",
+      "--with-cpu=generic",
+      "--libdir={prefix}/lib",
+      "--libexecdir={prefix}/lib",
+      "--build={target_arch}",
+      "--host={target_arch}",
+      "--target={target_arch}",
+    ]
+
+    @env = {
+      "CC" => "gcc -std=gnu89",
+      "CXX" => "g++ -std=gnu++98",
+      "CFLAGS" => "-w -O2 -fomit-frame-pointer -pipe -fPIC",
+      "CXXFLAGS" => "-w -O2 -fomit-frame-pointer -pipe -fPIC",
+    }
+  end
+
+  def do_install
+    @pkginfo_file = File.join(@pkginfo_dir, @pkgname + ".info")
+
+    # Replace '{prefix}' on configure parameters.
+    @conf_options.each_with_index do |co, ind|
+      if co.include? "{prefix}"
+        @conf_options[ind] = co.gsub("{prefix}", @prefix)
+      end
+      if co.include? "{target_arch}"
+        @conf_options[ind] = co.gsub("{target_arch}", @os_type)
+      end
+    end
+    @env.each do |key, flag|
+      if flag.include? "{prefix}"
+        @env[key] = flag.gsub("{prefix}", @prefix)
+      end
+    end
+
+    if File.file?(@pkginfo_file)
+      puts "Oh, it seems gcc was already installed!! Skipping!!"
+      return 0
+    end
+
+    puts "Downloading src from #{@source_url}"
+    dl = Download.new(@source_url, @src_dir)
+    source_file = dl.GetPath()
+    fp = FNParser.new(source_file)
+    src_tarball_fname, src_tarball_bname = fp.name
+
+    extracted_src_dir = File.join(@build_dir, src_tarball_bname)
+    bld_dir = extracted_src_dir + "-build"
+    @src_build_dir = bld_dir
+
+    if Dir.exists?(extracted_src_dir)
+      puts "Extracted folder has been found. Using it!"
+    else
+      puts "Extracting..."
+      self.Run("tar xf " + source_file + " -C " + @build_dir)
+    end
+
+    unless Dir.exists?(@prefix)
+      FileUtils.mkdir_p(@prefix)
+    end
+
+    # Downloading prerequisites
+    # puts extracted_src_dir
+    # self.Run("cd " + File.realpath(extracted_src_dir) + " && " + "./contrib/download_prerequisites")
+
+    # Need to patch a few files.
+    puts ""
+    puts "Patching bugged files..."
+    decl_c_patch = \
+%q{--- ./decl.c	2003-04-29 14:51:56.000000000 -0400
++++ ./decl.c	2021-11-09 14:58:11.262463175 -0500
+@@ -437,9 +437,9 @@
+ /* The binding level currently in effect.  */
+ 
+ #define current_binding_level			\
+-  (cfun && cp_function_chain->bindings		\
+-   ? cp_function_chain->bindings		\
+-   : scope_chain->bindings)
++  (*(cfun && cp_function_chain->bindings	\
++   ? &cp_function_chain->bindings		\
++   : &scope_chain->bindings))
+ 
+ /* The binding level of the current class, if any.  */
+ }
+
+    # Writing decl.c patch file
+    File.open("#{extracted_src_dir}/decl_c.patch", 'w') { |f| f.write decl_c_patch }
+
+    patch_cmd = [
+      "sed -i -e 's/\\*((void \\*\\*)__o->next_free)++ = ((void \\*)datum);/\\*((void \\*\\*)__o->next_free) = ((void \\*)datum);__o->next_free += sizeof(void \\*);/g' #{extracted_src_dir}/include/obstack.h",
+      "cd #{extracted_src_dir}/gcc/cp",
+      "patch #{extracted_src_dir}/gcc/cp/decl.c < #{extracted_src_dir}/decl_c.patch"
+    ]
+    # patch_cmd = [
+    #   "sed -i -e 's/__attribute__/\\/\\/__attribute__/g' #{extracted_src_dir}/gcc/cp/cfns.h",
+    #   "sed -i 's/struct ucontext/ucontext_t/g' #{extracted_src_dir}/libgcc/config/i386/linux-unwind.h",
+    #   "sed -i '/#include <pthread.h>/a #include <signal.h>' #{extracted_src_dir}/libsanitizer/asan/asan_linux.cc",
+    #   "sed -i 's/__res_state \\*statp = (__res_state\\*)state\\;/struct __res_state \\*statp = (struct __res_state\\*)state\\;/g' #{extracted_src_dir}/libsanitizer/tsan/tsan_platform_linux.cc",
+    # ]
+    # end
+    self.Run(patch_cmd.join(" && "))
+
+    # Let's build!!
+    unless Dir.exists?(bld_dir)
+      puts "Build dir missing... making one..."
+    else
+      puts "Build dir exists, cleaning up before work!!"
+      FileUtils.rm_rf(bld_dir)
+    end
+    FileUtils.mkdir_p(bld_dir)
+
+    if @need_sudo
+      inst_cmd = "sudo make install"
+    else
+      inst_cmd = "make install"
+    end
+
+    opts = ["--prefix=" + @prefix] + @conf_options
+    cmd = [
+      self.get_env_str,
+      "cd #{File.realpath(bld_dir)}",
+      "#{File.join(File.realpath(extracted_src_dir), "configure")} #{opts.join(" ")}",
+      "nice make -j #{@Processors.to_s}",
+      inst_cmd,
+    ]
+
+    brag_msg = %{
+
+*** This is totally deprecated software! ***
+*** If it breaks, it breaks... ***
+*** (This package was added solely due to Cadence ***
+
+}
+    puts brag_msg
+    sleep(2)
+
+    # Ok let's rock!
+    puts "Compiling (with #{@Processors} processors) and Installing ..."
+    self.RunInstall(env: @env, cmd: cmd.join(" && "))
+
+    self.WriteInfo
+  end
+end # class InstGCC4Sen
+
