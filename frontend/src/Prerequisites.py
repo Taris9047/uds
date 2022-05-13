42b49e21691e54bcc7c95bb890a8b75de53728ac
Taylor Shin <talezshin@gmail.com>
2022-05-13 10:39:48 -0400

    "Updated UDS for Fedora36"

--- frontend/src/Prerequisites.py
+++ frontend/src/Prerequisites.py
@@ -276,6 +276,9 @@ class InstallPrereqPkgs(GetPackages, RunCmd):
     def install_prereq_fedora_35(self):
         self.install_prereq_fedora_33()
 
+    def install_prereq_fedora_36(self):
+        self.install_prereq_fedora_33()
+
     def install_prereq_almalinux_8(self):
         # print("Almalinux detected! Activating CentOS repo!")
         self.install_prereq_centos_8()
