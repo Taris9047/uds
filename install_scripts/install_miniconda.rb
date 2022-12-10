8e62f37c30a374c8e1821a8e920dab7eb10d1b3a
Taylor Shin <talezshin@gmail.com>
2022-12-11 05:39:17 +0900

    Implemented miniconda script...
    And added neofetch to RHEL 9...

---
+++ install_scripts/install_miniconda.rb
@@ -0,0 +1,51 @@
+#!/usr/bin/env ruby
+
+# Miniconda install script...
+# Using... official miniconda install script for Linux x86_64
+#
+
+require_relative '../utils/utils.rb'
+require_relative './install_stuff.rb'
+
+class InstMiniconda < InstallStuff
+
+  def initialize(args)
+    args.each do |k,v|
+      instance_variable_set("@#{k}", v) unless v.nil?
+    end
+
+    super(@pkgname, @prefix, @work_dirs, @ver_check, @verbose_mode)
+
+    @source_url = SRC_URL[@pkgname]
+
+    # Setting up compilers
+    self.CompilerSet
+
+  end
+
+  def do_install
+
+    puts ""
+    puts "Working on #{@pkgname}"
+    puts ""
+
+    dl = Download.new(@source_url, @src_dir)
+    src_path = dl.GetPath
+    conda_install_path = File.join(ENV['HOME'], '.miniconda3')
+    shell_name = `echo -e #{ENV['SHELL']}`.split('/')[-1].chomp
+
+    # Ok let's roll!!
+    cmds = [
+      "chmod +x #{src_path}", "&&",
+      "#{src_path}"
+    ]
+
+    puts "Running Miniconda installation script!!"
+    system("#{cmds.join(" ")} -b -p #{conda_install_path} -f")
+
+    puts "Activating Conda"
+    system("eval \"$(#{conda_install_path}/bin/conda shell.#{shell_name} hook)\"")
+    system("conda init")
+  end
+
+end # class InstMiniconda
